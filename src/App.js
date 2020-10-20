3f6bf6739bcf87e1a415c3148a84e57b8c0a66c9
Gabriel Pedro B Piazza <pedro.piazza99@gmail.com>
2020-10-20 13:13:38 -0300

    Chore: The input search its implemented and i  changed some backgrounds colors to be prettyer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { 
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
-import axios from 'axios';
-
 
 import Header from './components/Header';
 import Home from './pages/Home';
 import Inspect from './pages/Inspect';
 
 export default function App() {
-  const [pokemns, setPokemons] = useState(null);
-  
-  // useEffect(() => {
-  //   const request = axios.get("https://pokeapi.co/api/v2/pokemon?limit=893");
-	// 	request.then(anwser => {
-  //     setPokemons(anwser.data.results)
-  //   });
-  // },[]);
+
  
   return (
       <>
@@ -27,10 +18,10 @@ export default function App() {
           <Header />
           <Switch>
             <Route path="/pokemon/:id">
-              <Inspect pokemns={pokemns}/>
+              <Inspect />
             </Route>  
             <Route path="/">
-              <Home pokemns={pokemns}/> 
+              <Home /> 
             </Route>
           </Switch>
         </Router>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,41 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Card from '../components/Card';
 
 
-export default function Home(props) {
+export default function Home() {
   const [searchPokemon, setSearchPokemon] = useState('');
-  const {pokemns} = props;
+  const [pokemns, setPokemons] = useState([]);
+  
+  useEffect(() => {
+    const request = axios.get("https://pokeapi.co/api/v2/pokemon?limit=893");
+		request.then(anwser => {
+      console.log(anwser)
+      setPokemons(anwser.data.results)
+    });
+  },[]);
+
+  const search = (text) => {
+    setSearchPokemon(text)
+  }
+
+
+  if (pokemns.length === 0) {
+    return (<span className='loading__pokemons'>
+            <img src='img/pikachuDance.gif' />
+            Carregando Lista de Pokemons...
+            </span>)
+  }
+
+  const filtredPokemons = pokemns.filter(pokemn => pokemn.name.toLowerCase().includes(searchPokemon.toLowerCase()))
+
 
 
   return (
     <div className='home__container'>
-      <input onChange={e => setSearchPokemon(e.target.value)} value={searchPokemon} />
+      <input placeholder='Pesquise pelo nome' onChange={e => search(e.target.value)} value={searchPokemon} />
       <ul style={{listStyle: 'none'}}>
-        {pokemns ? 
-        pokemns.map((item)=><Card name={item.name} key={item.url.split('/')[6]} id={item.url.split('/')[6]} />) 
-        : 
-        <span className='loading__pokemons'>
-          <img src='img/pikachuDance.gif' />
-          Carregando Lista de Pokemons...
-        </span>}
+        {filtredPokemons.map((item)=><Card name={item.name} key={item.url.split('/')[6]} id={item.url.split('/')[6]} />) }
       </ul>
     </div>
   );
